8b9f3aad663dc9d19e6f7faea0d6606e64588d92
Tatsiana Kasperovich <tanyakasperovich@mail.ru>
2023-08-27 00:06:36 +0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Татьяна Касперович on 27.08.23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
